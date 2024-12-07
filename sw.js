8caaa7484f2163571c2bf908c5dce8f37a89ff52
Sriyan-Nakka <sriyannakka@gmail.com>
2024-12-07 19:04:25 +0530

    hopefully caching service worker

--- sw.js
+++ sw.js
@@ -1,27 +1,24 @@
 const staticCacheName = "site-static";
 const assets = [
   "/",
-  "./index.html",
-  "./style.css",
-  "./script.js",
-  "./Audio/workCycleComplete.wav",
-  "./Audio/breakCycleComplete.mp3",
-  "./Fonts/parkinsans-webfont.woff",
-  "./Fonts/parkinsans-webfont.woff2",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/Audio/workCycleComplete.wav",
+  "/Audio/breakCycleComplete.mp3",
+  "/Fonts/parkinsans-webfont.woff",
+  "/Fonts/parkinsans-webfont.woff2",
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("static").then((cache) => {
+    caches.open("static").then(cache => {
       return cache.addAll([
-        "/",
-        "./index.html",
-        "./style.css",
-        "./script.js",
-        "./Audio/workCycleComplete.wav",
-        "./Audio/breakCycleComplete.mp3",
-        "./Fonts/parkinsans-webfont.woff",
-        "./Fonts/parkinsans-webfont.woff2",
+        [
+          "/","/index.html","/style.css","/script.js",
+          "/Audio/workCycleComplete.wav","/Audio/breakCycleComplete.mp3",
+          "/Fonts/parkinsans-webfont.woff","/Fonts/parkinsans-webfont.woff2",
+        ],
       ]);
     })
   );
