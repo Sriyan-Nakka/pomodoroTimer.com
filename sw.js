57c31dc9a522eedcf1787d054880708896e51574
Sriyan-Nakka <sriyannakka@gmail.com>
2024-12-08 10:56:36 +0530

    added allAll to the add

--- sw.js
+++ sw.js
@@ -1,12 +1,12 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("static").then(cache => {
-      cache.add('/PomodoroTimer/index.html');
-      cache.add('/PomodoroTimer/script.js');
-      cache.add('/PomodoroTimer/Audio/workCycleComplete.wav');
-      cache.add('/PomodoroTimer/Audio/breakCycleComplete.mp3');
-      cache.add('/PomodoroTimer/Fonts/parkinsans-webfont.woff');
-      cache.add('/PomodoroTimer/Fonts/parkinsans-webfont.woff2');
+      cache.addAll(['/PomodoroTimer/index.html']);
+      cache.addAll(['/PomodoroTimer/script.js']);
+      cache.addAll(['/PomodoroTimer/Audio/workCycleComplete.wav']);
+      cache.addAll(['/PomodoroTimer/Audio/breakCycleComplete.mp3']);
+      cache.addAll(['/PomodoroTimer/Fonts/parkinsans-webfont.woff']);
+      cache.addAll(['/PomodoroTimer/Fonts/parkinsans-webfont.woff2']);
     })
   );
 });
